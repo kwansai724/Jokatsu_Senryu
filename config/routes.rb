393da8f033cdec34634912541bfe7861de81d6a5
kwansai724 <kwansai.taka724@gmail.com>
2021-10-14 00:02:11 +0900

    rootを新規登録画面に設定、新規登録、ログイン画面のデザイン調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  # rootを新規登録画面に設定
+  devise_scope :voter do
+    root "voters/registrations#new"
+  end
+
   devise_for :voters, :controllers => {
     :registrations => 'voters/registrations',
     :sessions => 'voters/sessions'
   }
 
-  root 'homes#index'
+  # root 'homes#index'
 end
