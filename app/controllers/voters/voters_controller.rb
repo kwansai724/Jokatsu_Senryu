478c5d47a332a29e33566bccd9a66b4e0f60b674
kwansai724 <kwansai.taka724@gmail.com>
2021-10-15 21:18:17 +0900

    投票画面、管理者画面の閲覧制限設定

--- app/controllers/voters/voters_controller.rb
+++ app/controllers/voters/voters_controller.rb
@@ -1,6 +1,7 @@
 class Voters::VotersController < ApplicationController
   before_action :authenticate_voter!
   before_action :admin_only, only: [:admin]
+  before_action :voter_only, only: [:index]
 
   def index
     @voter = current_voter
