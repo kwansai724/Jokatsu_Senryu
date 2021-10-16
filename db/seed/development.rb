7b683fa4add5d971c96868a204e9c9f7490da015
kwansai724 <kwansai.taka724@gmail.com>
2021-10-16 15:31:49 +0900

    ナビゲーションバー削除、背景画像設定（仮）、サンプル投票者作成

--- db/seed/development.rb
+++ db/seed/development.rb
@@ -7,10 +7,102 @@ Voter.create!(
   group: "administrator",
   admin: true,
   password: "password",
-  password_confirmation: "password",
+  password_confirmation: "password"
 )
-
 puts "管理者アカウント作成"
+
+
+# 投票者(WIP)
+5.times do |n|
+  name  = Faker::Name.name
+  email = "voter-#{n+1}@email.com"
+  group = "WIP"
+  favorite_post = Faker::Number.between(from: 1, to: 5000)
+  reason = Faker::Lorem.sentence
+  admin = "false"
+  password = "password"
+  Voter.create!(
+    name: name,
+    email: email,
+    group: group,
+    favorite_post: favorite_post,
+    reason: reason,
+    admin: admin,
+    password: password,
+    password_confirmation: password
+  )
+end
+puts "投票者(WIP)作成"
+
+
+# 投票者(株式会社A)
+5.times do |n|
+  name  = Faker::Name.name
+  email = "voter-#{n+6}@email.com"
+  group = "株式会社A"
+  favorite_post = Faker::Number.between(from: 1, to: 5000)
+  reason = Faker::Lorem.sentence
+  admin = "false"
+  password = "password"
+  Voter.create!(
+    name: name,
+    email: email,
+    group: group,
+    favorite_post: favorite_post,
+    reason: reason,
+    admin: admin,
+    password: password,
+    password_confirmation: password
+  )
+end
+puts "投票者(株式会社A)作成"
+
+
+# 投票者(株式会社B)
+5.times do |n|
+  name  = Faker::Name.name
+  email = "voter-#{n+11}@email.com"
+  group = "株式会社B"
+  favorite_post = Faker::Number.between(from: 1, to: 5000)
+  reason = Faker::Lorem.sentence
+  admin = "false"
+  password = "password"
+  Voter.create!(
+    name: name,
+    email: email,
+    group: group,
+    favorite_post: favorite_post,
+    reason: reason,
+    admin: admin,
+    password: password,
+    password_confirmation: password
+  )
+end
+puts "投票者(株式会社B)作成"
+
+
+# 投票者(株式会社C)
+5.times do |n|
+  name  = Faker::Name.name
+  email = "voter-#{n+16}@email.com"
+  group = "株式会社C"
+  favorite_post = Faker::Number.between(from: 1, to: 5000)
+  reason = Faker::Lorem.sentence
+  admin = "false"
+  password = "password"
+  Voter.create!(
+    name: name,
+    email: email,
+    group: group,
+    favorite_post: favorite_post,
+    reason: reason,
+    admin: admin,
+    password: password,
+    password_confirmation: password
+  )
+end
+puts "投票者(株式会社C)作成"
+
 
 # 投稿者
 35.times do |n|
@@ -30,8 +122,7 @@ puts "管理者アカウント作成"
     profession: profession,
     age: age,
     note: note,
-    questionary: questionary,
+    questionary: questionary
   )
 end
-
 puts "投稿者作成"
