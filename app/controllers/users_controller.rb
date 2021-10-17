008eba2b3609b7dbc0049b790810410f2f1cc26e
ahimi <testtestabe07@gmail.com>
2021-10-17 17:58:32 +0900

    posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    byebug
     @user = User.new(user_params)
     if @user.save
       flash[:success] = '投稿に成功しました。'
       redirect_to users_index_path
     else
-      render :index
+      flash[:danger] = "登録は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+      redirect_to users_index_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,17 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   accepts_nested_attributes_for :posts, allow_destroy: true
 
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :gender, presence: true
+  validates :address, presence: true
+  validates :profession, presence: true
+  validates :age, presence: true
+  validates :questionary, presence: true
+  
   # 投稿者一覧の検索機能
   scope :search, -> (search_params) do
     return if search_params.blank?
