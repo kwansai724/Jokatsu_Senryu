8fa59410c318ca3120539c69c05ede3b2685e749
kwansai724 <kwansai.taka724@gmail.com>
2021-10-19 22:45:20 +0900

    リレーション修正

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,11 +2,6 @@ class Voter < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :likes
-  has_many :favorites, through: :likes, source: :post
-
-  def like_this(clicked_post)
-    self.likes.find_or_create_by(post: clicked_post)
-  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
