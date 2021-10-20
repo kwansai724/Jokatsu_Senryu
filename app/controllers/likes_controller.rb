5fa1c33e95ed8b6bd4b12658687dd04179076fb4
kwansai724 <kwansai.taka724@gmail.com>
2021-10-20 18:25:01 +0900

    likes_controller修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,2 @@
 class LikesController < ApplicationController
-
-  def create
-    if current_voter.like_this(clicked_post)
-      flash[:success] = '投稿に「いいね！」しました。'
-      redirect_back(fallback_location: posts_index_path)
-    end
-  end
-
-  def destroy
-    current_voter.likes.find_by(post_id: params[:post_id]).destroy
-    flash[:danger] = '「いいね！」を解除しました。'
-    redirect_back(fallback_location: posts_index_path)
-  end
-
-  private
-
-  def clicked_post
-    Post.find(params[:post_id])
-  end
 end
