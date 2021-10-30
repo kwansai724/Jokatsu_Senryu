4c04dec046a45dc1bbe46f73d3e216f509aafd31
ahimi <testtestabe07@gmail.com>
2021-10-31 01:31:05 +0900

    voter_post

--- app/models/voterpost.rb
+++ app/models/voterpost.rb
@@ -1,9 +1,3 @@
 class Voterpost < ApplicationRecord
-  # belongs_to :voter
-  # has_many :voters, through: :likes
   belongs_to :voter
-  # accepts_nested_attributes_for :voter, allow_destroy: true
-  has_many :likes, dependent: :destroy
-  
-  # accepts_nested_attributes_for :likes
 end
