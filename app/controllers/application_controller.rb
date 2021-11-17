a4cde95ec86e489386f20614afd86843836afbda
kwansai724 <kwansai.taka724@gmail.com>
2021-11-17 22:11:49 +0900

    iframe表示設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :allow_iframe
+  protect_from_forgery with: :null_session
 
   # 投票者画面閲覧制限
   def voter_only
