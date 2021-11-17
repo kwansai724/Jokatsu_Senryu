2d1b2de2f4fe8dfe23b72216fd351d551f2eb2f5
kwansai724 <kwansai.taka724@gmail.com>
2021-11-17 23:19:55 +0900

    応募フォーム修正

--- app/controllers/staffs/staffs_controller.rb
+++ app/controllers/staffs/staffs_controller.rb
@@ -9,11 +9,11 @@ class Staffs::StaffsController < ApplicationController
   end
 
   def index
-    # @posts_index = Post.eager_load(:user).where(category: params[:category_name]).paginate(page: params[:page],per_page: 100)
+    @posts_index = Post.eager_load(:user).where(category: params[:category_name]).paginate(page: params[:page],per_page: 100)
     # @posts_index = Post.eager_load(:likes).where(category: params[:category_name], likes: {voter_id: Voter.where(group: params[:group_name]).ids})
     #                 .order('count(voter_id) desc').group('posts.id').paginate(page: params[:page],per_page: 100)
-    @posts_index = Post.eager_load(:likes).where(category: params[:category_name], likes: {voter_id: Voter.where(group: params[:group_name]).ids})
-                      .order('count(likes.id) desc').group(:id).paginate(page: params[:page],per_page: 100)
+    # @posts_index = Post.eager_load(:likes).where(category: params[:category_name], likes: {voter_id: Voter.where(group: params[:group_name]).ids})
+    #                   .order('count(likes.id) desc').group(:id).paginate(page: params[:page],per_page: 100)
     @staffs = Staff.where(admin: false)
     @staff = Staff.find_by(group_name: params[:group_name])
     @users = User.eager_load(:posts).all
