5ee001252152a789b6cee42683b0a7e1e17edd0f
kwansai724 <kwansai.taka724@gmail.com>
2023-04-18 22:04:41 +0900

    rails app:updateタスクを実行してファイル更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_125430) do
+ActiveRecord::Schema.define(version: 2023_04_18_123044) do
 
   create_table "adminmessages", force: :cascade do |t|
     t.string "message"
