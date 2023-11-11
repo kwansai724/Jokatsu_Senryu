675e16dc9acb34eb4adc48eb8a91e54d185f1b34
kwansai724 <kwansai.taka724@gmail.com>
2023-11-11 19:20:22 +0900

    pumaファイルをdevとprdで分ける

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -4,28 +4,18 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
-
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-#
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile File.expand_path('tmp/pids/server.pid')
 
 # Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
+# Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
@@ -42,17 +32,17 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+worker_timeout 60
+
 rails_root = Dir.pwd
-# 本番環境のみデーモン起動
-if Rails.env.production?
-  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
-  stdout_redirect(
-    File.join(rails_root, 'log', 'puma.log'),
-    File.join(rails_root, 'log', 'puma-error.log'),
-    true
-  )
-  # デーモン
-  daemonize
-end
+bind "unix://#{rails_root}/tmp/sockets/puma.sock"
+
+pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+stdout_redirect(
+  File.join(rails_root, 'log', 'puma.log'),
+  File.join(rails_root, 'log', 'puma-error.log'),
+  true
+)
+# デーモン
+daemonize
