3879acb5350df644a67da413922da64337fd64b4
kwansai724 <kwansai.taka724@gmail.com>
2023-11-11 19:23:39 +0900

    本番用のpuma設定ファイル修正

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,48 +1,23 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+root_dir = '/var/www/Jokatsu_Senryu'
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "production" }
-
-# Specifies the `pidfile` that Puma will use.
-pidfile File.expand_path('tmp/pids/server.pid')
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
-
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count)
+threads min_threads_count, max_threads_count
 
 worker_timeout 60
 
-rails_root = Dir.pwd
-bind "unix://#{rails_root}/tmp/sockets/puma.sock"
+bind "unix://#{root_dir}/tmp/sockets/puma.sock"
 
-pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+environment 'production'
+
+pidfile File.expand_path('tmp/pids/server.pid')
+
 stdout_redirect(
-  File.join(rails_root, 'log', 'puma.log'),
-  File.join(rails_root, 'log', 'puma-error.log'),
+  File.expand_path('log/puma_access.log'),
+  File.expand_path('log/puma_error.log'),
   true
 )
-# デーモン
+
+plugin :tmp_restart
+
 daemonize
