0ccecb1a40ea55dcbe19ac1bd37ae4ed922747ae
kwansai724 <kwansai.taka724@gmail.com>
2023-11-28 22:44:16 +0900

    buildspec修正

---
+++ .codebuild/run_task.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+aws ecs run-task --cluster $CLUSTER --task-definition $TASK_ARN --launch-type FARGATE \
+  --network-configuration "awsvpcConfiguration={subnets=[$SUBNET_A_ID, $SUBNET_C_ID], securityGroups=[$SG_ID], assignPublicIp=ENABLED}" \
+  --overrides '{"containerOverrides": [{"name":"$API_CONTAINER","command":["rails", "db:migrate", "db:seed"]}]}'
