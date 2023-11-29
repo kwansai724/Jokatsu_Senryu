9d6c9297cafd3d5fe578be76a5a9aa177c5ad90a
kwansai724 <kwansai.taka724@gmail.com>
2023-11-29 20:36:06 +0900

    デプロイテスト

--- .codebuild/run_ecs_task.sh
+++ .codebuild/run_ecs_task.sh
@@ -2,4 +2,4 @@
 
 aws ecs run-task --cluster $CLUSTER --task-definition $TASK_ARN --launch-type FARGATE \
   --network-configuration "awsvpcConfiguration={subnets=[$SUBNET_A_ID, $SUBNET_C_ID], securityGroups=[$SG_ID], assignPublicIp=ENABLED}" \
-  --overrides '{"containerOverrides": [{"name":"dev-jokatsu-api","command":["rails", "db:environment:set", "RAILS_ENV=production", "rails", "db:migrate:reset", "db:seed"]}]}'
+  --overrides '{"containerOverrides": [{"name":"dev-jokatsu-api","command":["rails", "db:environment:set", "RAILS_ENV=production", "&&", "rails", "db:migrate:reset", "db:seed", "&&", "rails" "s"]}]}'
